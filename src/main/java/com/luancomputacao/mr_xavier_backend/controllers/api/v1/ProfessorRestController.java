c9ec66355790c405d86b043c934323383e36be7f
LuanComputacao <eng.luanroger@gmail.com>
2019-08-27 14:03:36 -0300

    listing questions

--- src/main/java/com/luancomputacao/mr_xavier_backend/controllers/api/v1/ProfessorRestController.java
+++ src/main/java/com/luancomputacao/mr_xavier_backend/controllers/api/v1/ProfessorRestController.java
@@ -1,6 +1,6 @@
 package com.luancomputacao.mr_xavier_backend.controllers.api.v1;
 
-import com.luancomputacao.mr_xavier_backend.services.FaseDeEnsinoService;
+import com.luancomputacao.mr_xavier_backend.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/fase-de-ensino")
-public class FaseDeEnsinoRestController {
+@RequestMapping(value = "/professor")
+public class ProfessorRestController {
 
     @Autowired
-    FaseDeEnsinoService faseDeEnsinoService;
+    ProfessorService professorService;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(faseDeEnsinoService.findById(id));
+        return ResponseEntity.ok(professorService.findById(id));
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,6 @@
-insert into disciplina
-  (nome)
-values
-  ('Matemática'),
-  ('Português'),
-  ('Física'),
-  ('Biologia'),
-  ('Quimica'),
-  ('História'),
-  ('Sociologia'),
-  ('Filosofia'),
-  ('Inglês');
-
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Fases de Ensino
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
 insert into fase_de_ensino
   (nome)
 values
@@ -27,16 +17,57 @@ values
   ('2° Ano - Ensino Médio'),
   ('3° Ano - Ensino Médio');
 
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Matérias
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+insert into materia
+ (nome, id_disciplina)
+ values
+  ('Funções de Primeiro Grau', 1),
+  ('Funções de Segundo Grau', 1),
+  ('Verbos', 2),
+  ('Predicados', 2);
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Matérias
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+insert into disciplina
+  (nome)
+values
+  ('Matemática'),
+  ('Português'),
+  ('Física'),
+  ('Biologia'),
+  ('Quimica'),
+  ('História'),
+  ('Sociologia'),
+  ('Filosofia'),
+  ('Inglês');
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Professores
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
 insert into professor
   (cpf, moderador, nome, senha)
 values
   ('00000000000', 0, 'Teste 00', 'asdf1234'),
   ('07702439670', 1, 'Luan R. S. Santana', 'mrxavier123');
 
-
-
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Questões
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
 insert into questao
   (enunciado, espacos, invalidada, nivel, publica, tipo_de_questao, id_autor, id_disciplina, id_fase_de_ensino, criado_em, atualizado_em)
 values
   ('Esta é a terceira questão?', 3, 0, 1, 1, 1, 1, 1, 8, NOW(), NOW()),
   ('Esta é a segunda questão?', 3, 0, 2, 0, 1, 2, 2, 8, NOW(), NOW());
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Matérias das Questões
+-- - - - - - - - - - - - - - - - - - - - - - - - - -
+insert into materia_de_questao
+  (questao_id, materia_id)
+values
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (2, 4);
