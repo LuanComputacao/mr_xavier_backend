284cb99717db9e9dce91a3a90939e82143f708af
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-08-27 18:48:00 -0300

    get disciplinas

--- src/main/java/com/luancomputacao/mr_xavier_backend/services/DisciplinaService.java
+++ src/main/java/com/luancomputacao/mr_xavier_backend/services/DisciplinaService.java
@@ -2,9 +2,11 @@ package com.luancomputacao.mr_xavier_backend.services;
 
 import com.luancomputacao.mr_xavier_backend.domain.Disciplina;
 import com.luancomputacao.mr_xavier_backend.repositories.DisciplinaRepository;
+import com.luancomputacao.mr_xavier_backend.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +15,14 @@ public class DisciplinaService {
     @Autowired
     DisciplinaRepository disciplinaRepository;
 
-    public Optional<Disciplina> findById(Integer id) {
-        Optional<Disciplina> obj = disciplinaRepository.findById(id);
-        return obj;
+    public Disciplina findById(Integer id) {
+        Optional<Disciplina> disciplina = disciplinaRepository.findById(id);
+        return disciplina.orElseThrow(() -> new ObjectNotFoundException("Professor não encontrado"));
     }
+
+    public List list() {
+        Optional<List> disciplinas = Optional.of(disciplinaRepository.findAll());
+        return disciplinas.orElseThrow(() -> new ObjectNotFoundException("Professores não encontrados"));
+    }
+
 }
