bfe7e8f8859396d84edfd90fdd4c397c612c5e13
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-10-23 19:21:36 -0300

    post

--- src/main/java/com/luancomputacao/mr_xavier_backend/controllers/api/v1/ProfessorRestController.java
+++ src/main/java/com/luancomputacao/mr_xavier_backend/controllers/api/v1/ProfessorRestController.java
@@ -1,12 +1,13 @@
 package com.luancomputacao.mr_xavier_backend.controllers.api.v1;
 
+import com.luancomputacao.mr_xavier_backend.domain.Professor;
 import com.luancomputacao.mr_xavier_backend.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/professor")
@@ -23,5 +24,17 @@ public class ProfessorRestController {
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Integer id) {
         return ResponseEntity.ok(professorService.findById(id));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> insert(@RequestBody Professor obj) {
+        obj = professorService.insert(obj);
+        return getResponseEntity(obj);
+    }
+
+    private ResponseEntity<?> getResponseEntity(@RequestBody Professor obj) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
